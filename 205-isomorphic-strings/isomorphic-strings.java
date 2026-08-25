b189ec00f892dbf3213181de569b115338e82ab0
YASHANK-SHARMA <yashank731@gmail.com>
2026-08-25 10:32:56 +0530

    Time: 5 ms (98.71%) | Memory: 43.5 MB (93.94%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] sMap = new int[256];
+        int[] tMap = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+            if (sMap[a] != tMap[b]) {
+                return false;
+            }
+            sMap[a] = i + 1;
+            tMap[b] = i + 1;
+        }
+        return true;
+    }
+}
