e1342a5622d35e55387731e23c15db79dd8d622e
YASHANK-SHARMA <yashank731@gmail.com>
2026-09-16 15:27:40 +0530

    Time: 3 ms (99.97%) | Memory: 79.4 MB (99.96%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,39 +1,29 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        int n = nums.length;
-        int left = 0, right = 0;
-        long maxSum = 0;
-        long sum = 0;
-        HashMap<Integer, Integer> mpp = new HashMap<>();
-        for(int i = 0; i<k; i++)
-        {
-            sum += nums[right];
-            mpp.put(nums[right], mpp.getOrDefault(nums[right], 0) + 1);
-            right++;
+        int max = Integer.MIN_VALUE;
+        for(int n : nums){
+            if(n > max ) max = n;
         }
-        if(mpp.size() == k)
-        {
-        maxSum = Math.max(maxSum, sum);
+        boolean freq[] = new boolean[max+1];
+        int start = 0;
+        long  sum = 0;
+        long  maxWeight = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+        while(freq[nums[i]]==true){
+            freq[nums[start]]=false;
+            sum-=nums[start];
+            start++;
         }
-        for(right = k; right<n; right++)
-        {
-            sum += nums[right];
-            sum -= nums[left];
-            mpp.put(nums[right], mpp.getOrDefault(nums[right], 0) + 1);
-            mpp.put(nums[left], mpp.get(nums[left]) - 1);
-            if(mpp.get(nums[left]) == 0)
-            {
-                mpp.remove(nums[left]);
-            }
-            left++;
-
-            if(mpp.size() == k && right-left+1 == k)
-            {
-                maxSum = Math.max(sum, maxSum);
-            }
+        freq[nums[i]]=true;
+        sum+=nums[i];
+        if((i - start) + 1 == k){
+            maxWeight = Math.max(maxWeight, sum);
+            freq[nums[start]]=false;
+            sum-=nums[start];
+            start++;
         }
-        return maxSum;
-
-
+        }
+        if(maxWeight == Integer.MIN_VALUE ) return 0;
+        return maxWeight;
     }
 }
